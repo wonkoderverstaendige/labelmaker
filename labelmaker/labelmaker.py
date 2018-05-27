8f2b872369d23f722119d3d6306c7e594edc003b
Ronny Eichler <R.Eichler@science.ru.nl>
2018-05-27 03:24:37 +0200

    Basic functionality two label two conspecifics

---
+++ labelmaker/labelmaker.py
@@ -0,0 +1,171 @@
+import cv2
+import math
+import time
+import numpy as np
+import pandas as pd
+import argparse
+from datetime import datetime
+from pathlib import Path
+
+MODES = ['HA', 'HP', 'VA', 'VP']
+MODE_COLORS = {'HA': (210, 100, 100), 'HP': (255, 128, 128), 'VA': (100, 210, 100), 'VP': (128, 255, 128)}
+COLUMNS = ['presented', 'label',
+           'HAx', 'HAy', 'HPx', 'HPy',
+           'VAx', 'VAy', 'VPx', 'VPy']
+
+class LabelMaker:
+    def __init__(self, vid_path, labels_file=None, start_frame=0, frame_skip=2, force_mode=None):
+        self.__start_frame = start_frame
+
+        self.path = Path(vid_path).resolve()
+        if not self.path.exists():
+            raise FileNotFoundError(str(self.path))
+
+        self.capture = cv2.VideoCapture(str(self.path))
+
+        self.num_frames = int(self.capture.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.frame_width = int(self.capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.frame_height = int(self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        self.frame = None
+        self.frame_id = None
+        self.disp_frame = None
+
+
+        self.labels_csv_path = Path(labels_file).resolve() if labels_file is not None else None
+        if self.labels_csv_path.exists():
+            print('Loading existing labels file')
+            self.labels = pd.read_csv(str(self.labels_csv_path), index_col='frame')
+        else:
+            print('Creating empty labels DF')
+            self.labels = pd.DataFrame(index=range(self.num_frames),
+                                       columns=COLUMNS)
+        self.labels['presented'] = 0
+
+        self.alive = True
+        self.pressed_key = -1
+        self.frame_skip = frame_skip
+
+        self.mouse_x = None
+        self.mouse_y = None
+
+        self.mode = 0
+        if force_mode is not None:
+            print(f'Forcing mode {force_mode}')
+            self.force_mode = int(force_mode)
+            self.mode = self.force_mode
+        else:
+            self.force_mode = None
+
+        cv2.namedWindow('LabelMaker')
+        cv2.setMouseCallback('LabelMaker', self.process_mouse_event)
+
+        self.time_last_grab = None
+        self.loop()
+
+    def loop(self):
+        self.grab(abs=0)
+        while self.alive:
+            self.disp_frame = self.frame.copy()
+
+            for m in MODES:
+                try:
+                    m_x = int(self.labels.loc[self.frame_id, m + 'x'])
+                    m_y = int(self.labels.loc[self.frame_id, m + 'y'])
+                except ValueError:
+                    pass
+                else:
+                    cv2.putText(self.disp_frame, text=m, org=(int(m_x), int(m_y)),
+                                fontFace=cv2.FONT_HERSHEY_PLAIN,
+                                fontScale=1.5,
+                                color=MODE_COLORS[m],
+                                lineType=cv2.LINE_AA,
+                                thickness=2)
+
+            cv2.imshow('LabelMaker', self.disp_frame)
+            key = cv2.waitKey(1000 // 30)
+            self.process_key(key)
+
+    def grab(self, rel=None, abs=None):
+
+        # if self.time_last_grab is not None:
+        #     print('{:.1f} s'.format(time.time() - self.time_last_grab))
+
+        #print('Before:', int(self.capture.get(cv2.CAP_PROP_POS_FRAMES)))
+        if rel is not None:
+            print(max(0, self.frame_id + rel))
+            self.capture.set(cv2.CAP_PROP_POS_FRAMES, max(0, self.frame_id + rel))
+        elif abs is not None:
+            self.capture.set(cv2.CAP_PROP_POS_FRAMES, max(0, abs))
+        else:
+            # skip n frames
+            for _ in range(self.frame_skip):
+                self.capture.read()
+        # read next frame
+        rv, frame = self.capture.read()
+        self.frame_id = int(self.capture.get(cv2.CAP_PROP_POS_FRAMES))
+
+        print('ID:', self.frame_id)
+
+        if rv:
+            self.frame = frame
+            self.labels.loc[self.frame_id, 'presented'] = 1
+            self.time_last_grab = time.time()
+
+        else:
+            if self.frame is None:
+                raise IOError('Frame acquisition failed')
+
+
+        self.mode = self.force_mode if self.force_mode else 0
+
+
+    def process_key(self, key):
+        if key < 0:
+            return
+        self.pressed_key = key
+
+        # Exit with q or Esc
+        if key in [27, ord('q')]:
+            self.quit()
+
+        # skip frame
+        elif key == ord('.'):
+            self.grab()
+
+        # skip frame
+        elif key == ord(','):
+            self.grab(rel=-self.frame_skip-1)
+
+    def process_mouse_event(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.labels.loc[self.frame_id, f'{MODES[self.mode]}x'] = x
+            self.labels.loc[self.frame_id, f'{MODES[self.mode]}y'] = y
+            self.mode = self.mode + 1
+            if self.mode >= len(MODES) or self.force_mode is not None:
+                self.grab()
+
+
+    def quit(self):
+        self.alive = False
+        cv2.destroyAllWindows()
+
+        # iso_date = datetime.now().strftime('%Y%m%d_%H%M%S')
+        # csv_out_path = str(self.path) + '.LabelMaker.csv'.format(iso_date)
+        self.labels.to_csv(self.labels_csv_path, index_label='frame')
+        print('Labels written to {}'.format(self.labels_csv_path))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path')
+    parser.add_argument('-l', '--labels', help='Path to labels csv file to load and update')
+    parser.add_argument('-M', '--force_mode', help='Force and process a single mode only')
+
+    cli_args = parser.parse_args()
+
+    LabelMaker(cli_args.path, labels_file=cli_args.labels, force_mode=cli_args.force_mode)
+
+
+if __name__ == '__main__':
+    main()
